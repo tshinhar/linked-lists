fae3a7c66d8c1a2310e357807ee0f1ffef4b39f1
tshinhar <tshinhar@gmail.com>
2020-03-09 14:58:21 +0200

    Add files via upload

---
+++ List.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+typedef struct node {
+	int data;
+	struct node *next
+}Node;
+
+Node* create_new_node(int value) {
+	Node* new_node = (Node*)malloc(sizeof(Node));
+	new_node->data = value;
+	new_node->next = NULL;
+}
+
+Node* add_to_list_start(Node* head, Node* new_node) {
+	new_node->next = head;
+	return new_node;
+}
+
+Node* add_to_list_end(Node* head, Node* new_node) {
+	if (head == NULL)
+		return new_node;
+	Node* current_node = head;
+	while (current_node->next != NULL)
+	{
+		current_node = current_node->next;
+	}
+	current_node->next = new_node;
+	return head;
+}
+
+Node* add_after_specific_node(Node* head, int value_target, Node* new_node) {
+	Node* current_node = head;
+	while (current_node->data != value_target) {
+		if (current_node == NULL) {
+			printf("Error - the taarget node is not in the list");
+			/// exit the program
+		}
+		current_node = current_node->next;
+	}
+	Node* helper_node = current_node->next;
+	current_node->next = new_node;
+	new_node->next = helper_node;
+	return head;
+}
+
+void print_node_index(Node* head, int value_target) {
+	Node* current_node = head;
+	int index = 0;
+	while (current_node->data != value_target) {
+		if (current_node == NULL) {
+			printf("%d", -1);
+			break;
+		}
+		current_node = current_node->next;
+		index++;
+	}
+	printf("%d", index);
+}
